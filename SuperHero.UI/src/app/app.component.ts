87ab7e51e5c2069971321d887b10c7f9a7cb05bb
Your Name <you@example.com>
2022-06-27 07:56:50 -0700

    a

---
+++ SuperHero.UI/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy } from '@angular/core';
+import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'SuperHero.UI';
+  heroes: SuperHero[] = [];
+  heroToEdit?: SuperHero;
+
+  /**
+   *
+   */
+  constructor(private superHeroService: SuperHeroService) {}
+
+  ngOnInit(): void {
+    this.superHeroService
+      .getSuperHeroes()
+      .subscribe((result: SuperHero[]) => (this.heroes = result));
+  }
+
+  initNewHero() {
+    this.heroToEdit = new SuperHero();
+  }
+
+  editHero(hero: SuperHero) {
+    this.heroToEdit = hero;
+  }
+
+  updateHeroList(heroes: SuperHero[]) {
+    this.heroes = heroes;
+  }
+  ngOnDestroy(): void {}
+}
